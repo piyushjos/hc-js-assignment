c233d0c179bf28623338973eb6125de8bd9e57a3
piyush <piyushj8989@gmail.com>
2022-03-01 17:33:34 +0530

    Task3 Using map(), filter(), reduce(), some() and every() Filter out odd even prime no

---
+++ js_moduel2/Task3.js
@@ -0,0 +1,43 @@
+// Provided the following array: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+// Filter out odd, even, and prime numbers and print them
+// 4. Provided the following array:
+
+var array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+function odd(x) {
+    if (x % 2 != 0) {
+        return x
+    }
+}
+var result = array.filter(odd)
+
+console.log(`${result} are odd numbers`);
+
+
+
+function even(y) {
+    if (y % 2 == 0) {
+        return y
+    }
+}
+var result1 = array.filter(even)
+
+console.log(`${result1} are even numbers `);
+
+function prime(a) {
+    if (a <= 1) {
+
+        return false
+    } else {
+        for (let i = 2; i < a; i++) {
+            if (a % i == 0) {
+                return false;
+            }
+
+        }
+        return true;
+    }
+
+}
+var result3 = array.filter(prime)
+console.log(result3)
