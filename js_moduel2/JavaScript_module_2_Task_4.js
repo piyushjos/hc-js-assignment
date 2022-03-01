bd86f4d826d67d46c5d08be3a831348bbf61aeb2
piyush <piyushj8989@gmail.com>
2022-03-01 17:44:00 +0530

    javaScript Module-2 Task-4 Completed

---
+++ js_moduel2/JavaScript_module_2_Task_4.js
@@ -0,0 +1,21 @@
+// Provided the following array: [0, 2, 4, 6, 8, 10, 12, 14, 16]
+// Generate and print another list such that:
+//     a.every element being twice of elements in the current list.
+
+var array = [0,
+    2, 4, 6, 8, 10, 12, 14, 16
+]
+
+function double(x) {
+    return x * 2;
+}
+
+const result = array.map(double);
+console.log(`${result} doubled value`);
+
+// b.every element being half of the elements in the current list.
+
+const result1 = array.map((y) => y / 2);
+console.log(`${
+    result1
+} half value`);
