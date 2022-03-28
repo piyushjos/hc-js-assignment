f6f563f5d890da3ffb29a58b3ad937c9c48335e9
piyush <piyushj8989@gmail.com>
2022-03-28 17:50:21 +0530

    module_9 task 2 completed

---
+++ Module_9/Task_2/promise1.js
@@ -0,0 +1,14 @@
+// Implement code to handle multiple promises.You have to fetch information of 5 different products from API
+async function init() {
+
+    const result = await Promise.allSettled([
+        fetch("https://fakestoreapi.com/prod").then((response) => response.json()),
+        fetch("https://fakestoreapi.com/products/2").then((response) => response.json()),
+        fetch("https://fakestoreapi.com/products/3").then((response) => response.json()),
+        fetch("https://fakestoreapi.com/products/4").then((response) => response.json()),
+        fetch("https://fakestoreapi.com/products/5").then((response) => response.json())
+    ]);
+    console.log(result)
+
+}
+init();
