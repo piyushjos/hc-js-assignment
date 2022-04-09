c31d5bb1c0b73099d4f4caef7ea145b6b09403cc
piyush <piyushj8989@gmail.com>
2022-04-09 17:05:13 +0530

    Task

---
+++ ecommerce_clone/src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>skjhsjkhjskhasjbsdbsdgh</h1>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+
+export default {
+  name: 'Home',
+  components: {
+  
+  }
+}
+</script>
