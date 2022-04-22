98e6353dac86f4304ee5dad2675bb5bbbbca825a
piyush <piyushj8989@gmail.com>
2022-04-22 17:35:14 +0530

    Update application labels to use internationalization

--- login-registration/src/components/Login.vue
+++ login-registration/src/components/Login.vue
@@ -3,99 +3,30 @@
     <div class="col-md-6 offset-md-3">
       <div class="container">
         <div class="heading">
-          <h3>Sign In</h3>
+          <h3>{{ $t('signIn') }}</h3>
           <hr />
         </div>
         <form>
           <div class="form-group">
-            <label>Username</label>
+            <label>{{ $t('userName') }}</label>
             <input type="text" class="form-control" v-model="username" />
           </div>
 
           <div class="form-group">
-            <label>Password</label>
+            <label>{{ $t('password') }}</label>
             <input type="password" class="form-control" v-model="password" />
           </div>
 
         <div>
          <input class="form-check-input mt-0" type="checkbox" id="check" required value=""/>
-         <span>Remember Me! </span>
+         <span>{{ $t('rememberMe') }}</span>
         </div>
 
           <div class="my-3">
-            <button @click="loginUser" type="submit" class="btn btn-primary">SignIn</button>
+            <button @click="loginUser" type="submit" class="btn btn-primary">{{ $t('signIn') }}</button>
           </div>
         </form>
       </div>
     </div>
   </div>
 </template>
-
-<script>
-import axios from "axios";
-
-
-export default {
-  data() {
-    return {
-      username: this.username,
-      password: this.password,
-    };
-  },
-
-  methods: {
-    loginUser(event) {
-      event.preventDefault();
-        
-      let preventCorsError = {
-        headers: {
-    
-          "Content-Type": "application/json",
-           Accept: "*/*",
-        }, 
-      };
-
-      this.postData(preventCorsError)
-    },
-
-    postData(preventCorsError) {
-      const credentials = {
-        username: this.username,
-        password: this.password
-      }
-
-      axios.post(
-        "https://fakestoreapi.com/auth/login",
-        
-        credentials,
-        preventCorsError
-      ).then((res) => {
-
-        localStorage.token = JSON.stringify(res.data.token);
-        console.log(res)
-        console.log("hello");
-
-
-        this.sendUserToHome(localStorage.token)
-        
-      }).catch((err) => {
-        alert(err);
-      })
-    },
-
-    sendUserToHome(token) {
-      if(token) {
-        this.$router.push('/')
-      } else {
-        alert("Unautorized user")
-      }
-    }
-  }
-};
-</script>
-
-<style>
-.error {
-  color: red;
-}
-</style>
--- login-registration/src/main.js
+++ login-registration/src/main.js
@@ -3,4 +3,27 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-createApp(App).use(store).use(router).mount('#app')
+import { createI18n } from 'vue-i18n'
+
+const i18n = createI18n({
+    locale: "hi",
+    fallbackLocale: "en",
+    messages: {
+        en: {
+            home: 'Home',
+            signIn: 'SignIn',
+            userName: 'Username',
+            password: 'Password',
+            rememberMe: 'Remember Me'
+        },
+        hi: {
+            home: 'घर',
+            signIn: 'साइन इन करें',
+            userName: 'उपयोगकर्ता नाम',
+            password: 'पासवर्ड',
+            rememberMe: 'मुझे याद रखें'
+        }
+    }
+})
+
+createApp(App).use(router).use(i18n).mount('#app')
